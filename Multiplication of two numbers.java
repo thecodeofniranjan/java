5a966c253a5b387236eefb28e39b4196085b5535
thecodeofniranjan <niranjankapale2004@gmail.com>
2025-06-10 00:35:40 +0530

    Create Multiplication of two numbers.java

---
+++ Multiplication of two numbers.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Code{
+    public static void main(String[] args){
+        Scanner NK=new Scanner(System.in);
+        
+        
+        // Multiplication of two numbers
+        System.out.print("Enter First number : ");
+      double a=NK.nextDouble();
+        
+        System.out.print("Enter Second number : ");
+      double b=NK.nextDouble();
+        
+        
+    double M=a*b;
+    System.out.print("The multiplication of " +a+ " and " +b+  " is: " + M);
+        
+    }
+}
