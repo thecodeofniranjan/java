5bd9e412f0a0fcc6f3e35002a09b2b882cbb2b1e
thecodeofniranjan <niranjankapale2004@gmail.com>
2025-06-09 11:52:28 +0530

    Create Conditional Statements.java

---
+++ Conditional Statements.java
@@ -0,0 +1,27 @@
+  import java.util.Scanner;
+   
+   public class Conditional {
+   public static void main(String[] args) {
+   Scanner NK = new Scanner(System.in);
+   System.out.println("Enter the age of user: ");
+   int a = NK.nextInt();
+   int b = NK.nextInt();
+   if (a == b) {
+   System.out.println("a is equal to b ");
+   
+   } 
+   else {
+   if (a > b) {
+   System.out.println("a is greater than  b ");
+   
+   }
+   else {
+   
+   System.out.println("a is lesser than b");
+   }
+   
+   }
+   
+   }
+   
+   }
