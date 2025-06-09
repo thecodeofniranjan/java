b9a2baaaad1ca80634746d5b52c356b4d1a064d0
thecodeofniranjan <niranjankapale2004@gmail.com>
2025-06-09 12:04:03 +0530

    Create sum of 1st 100 numbers.java

---
+++ sum of 1st 100 numbers.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Niranjan {
+    public static void main(String[] args) {
+        Scanner NK = new Scanner(System.in);
+        int sum=0;
+
+        for (int i=0; i<100; i++) {
+            sum=sum+i;
+        }
+         System.out.println(sum);
+    }
+
+}
