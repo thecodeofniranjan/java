85961411c71fcc4601f25eafac836ce9aa390746
thecodeofniranjan <niranjankapale2004@gmail.com>
2025-06-09 11:54:20 +0530

    Create Addition of two no. by taking input from user.java

---
+++ Addition of two no. by taking input from user.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+
+class niranjan {
+    public static void main(String[] args) {
+        Scanner NK = new Scanner(System.in);
+        System.out.println("enter the value of a:");
+        int a = NK.nextInt();
+        System.out.println("enter the value of B:");
+        int B = NK.nextInt();
+        System.out.println(" the value of sum of this two numbers is:");
+        int sum = a + B;
+        System.out.println(sum);
+
+    }
+}
