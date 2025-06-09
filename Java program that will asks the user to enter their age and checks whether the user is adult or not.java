3a252e6a8d1d18c2acabd98f64e03f8880f8ae68
thecodeofniranjan <niranjankapale2004@gmail.com>
2025-06-09 11:40:12 +0530

    Create Java program that will asks the user to enter their age and checks whether the user is adult or not.java

---
+++ Java program that will asks the user to enter their age and checks whether the user is adult or not.java
@@ -0,0 +1,16 @@
+  import java.util.Scanner;
+  
+  public class Conditional {
+  public static void main(String[] args) {
+  Scanner NK = new Scanner(System.in);
+  System.out.println("Enter the age of user: ");
+  int age = NK.nextInt();
+  if (age > 18) {
+  System.out.println("Conragtualations you are adult ");
+  }
+  else {
+ System.out.println("You are not adult");
+  }
+  
+  }
+ } 
