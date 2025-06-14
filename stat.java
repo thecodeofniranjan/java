4babcdb5b7b3b85360689db3545a8cf67aa286fd
thecodeofniranjan <niranjankapale2004@gmail.com>
2025-06-14 12:36:09 +0530

    Create stat.java
    I created a class Stat which contains a static method printInfo() that prints a message inside the main() method, we called printInfo() directly without creating an object because it's static
     This shows how to call a method in the main function

---
+++ stat.java
@@ -0,0 +1,18 @@
+public class Stat {
+// static method
+// calling one method into main function
+      public static void printInfo() {  // this is the method
+            System.out.println("It is a Function");
+      }
+
+      public static void main(String[] args) {
+            printInfo();// method or Function called
+      }
+
+}
+
+
+
+
+
+// output: It is a Function\
